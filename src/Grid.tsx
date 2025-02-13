efa2cc61da34c302adbdcbdfc5766e848ab8d2e6
Angelo Mancilla Mendoza <87000518+ApoloArc@users.noreply.github.com>
2025-02-12 19:44:26 -0500

    Task #6

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -3,6 +3,8 @@ import { ColDef } from "ag-grid-community";
 import data from "./near-earth-asteroids.json";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
+import { styles } from "./styles/Grid";
+import { useCallback, useRef } from "react";
 
 const columnDefs: ColDef[] = [
   { field: "designation", headerName: "Designation", filter: "agNumberColumnFilter" },
@@ -45,12 +47,25 @@ const columnDefs: ColDef[] = [
 ];
 
 const NeoGrid = (): JSX.Element => {
+  const gridRef = useRef<any>();
+
+  const clearFilters = useCallback(() => {
+    if (gridRef.current) {
+      gridRef.current.api.setFilterModel(null);
+    }
+  }, []);
+
   return (
     <div className="ag-theme-alpine" style={{ height: 900, width: 1920 }}>
       {/* Title */}
-      <h1>Near Earth Objects</h1>
+      <div style={styles.titleSectionContainer}>
+        <h1>Near Earth Objects</h1>
+        <button style={styles.button} onClick={clearFilters}>
+          Clear Filters and Sorters
+        </button>
+      </div>
       {/* Grid */}
-      <AgGridReact rowData={data} columnDefs={columnDefs} rowGroupPanelShow={"always"} enableCellTextSelection cellSelection />
+      <AgGridReact ref={gridRef} rowData={data} columnDefs={columnDefs} rowGroupPanelShow={"always"} enableCellTextSelection cellSelection />
     </div>
   );
 };
---
+++ src/styles/Grid.ts
@@ -0,0 +1,15 @@
+export const styles = {
+  titleSectionContainer: {
+    display: "flex",
+    gap: "15px",
+    marginBottom: "10px",
+    alignItems: "center",
+  },
+  button: {
+    backgroundColor: "white",
+    borderRadius: ".5rem",
+    height: "max-content",
+    padding: ".5rem",
+    cursor: "pointer",
+  },
+};
